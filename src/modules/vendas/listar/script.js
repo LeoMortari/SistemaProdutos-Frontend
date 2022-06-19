ac175a71c4446c984ac0043c5e7d7a1c9bbd2484
Lucas Kenzo <kenzoawane@gmail.com>
2022-06-19 16:39:15 -0300

    add modulo vendas (listar e editar)

---
+++ src/modules/vendas/listar/script.js
@@ -0,0 +1,38 @@
+//funcao para dar um efeito de carregamento de cada linha na tabela
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getVendas = async () => (await getHistoricoVendas())
+
+// lista todos as vendas
+const historicoVenda = async (userId) => {
+    for (i = 0; i < userId.length; i++) {
+        let data = new Date(userId[i].data)
+        let formatData = data.getHours() + ":"
+        + data.getMinutes() + " "
+        + (data.getDate() + 1) + "/" 
+        + (data.getMonth() + 1) + "/" 
+        + data.getFullYear();
+        await sleep(100)
+        let linha = `<tr onclick="paginaEditar(${userId[i].id_venda_pk})">
+            <td id="id${i}" value="${userId[i].id_venda_pk}">${userId[i].id_venda_pk}</td>
+            <td>${formatData}</td>
+            <td>${userId[i].vendedor}</td>   
+            <td>${userId[i].produtos}</td>    
+            <td>R$${userId[i].valor}</td>
+            </tr>`;
+        document.getElementById('tabBody').innerHTML += linha;
+    }
+};
+// vai para a pagina buscar por id
+function editarVenda(){
+    location.href = '../editar/editarVenda.html'
+}
+
+
+
+
+  
+
+
