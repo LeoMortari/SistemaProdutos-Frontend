a059eaf3f98334c518eb9f3c7b4ece49fd31fb47
Lucas Kenzo <kenzoawane@gmail.com>
2022-06-19 16:37:58 -0300

    remove push da master

--- src/modules/vendas/listar/api.js
+++
@@ -1,6 +0,0 @@
-const getHistoricoVendas = async () => {
-    fetch(`http://localhost:3000/vendas`)
-      .then((response) => response.json())
-      .then((result) => historicoVenda(result))
-      .catch((erro) => console.log("erro na request: " + erro))
-}
