05283f0ef4e93359c8342c8f503a90d4412d3748
GabrielPierote <101937415+GabrielPierote@users.noreply.github.com>
2022-06-26 22:01:23 -0300

    listar e editar

--- src/modules/Financeiro/api.js
+++ src/modules/Financeiro/api.js
@@ -1,61 +1,24 @@
 
- 
+
 
 
 //Função de submit da página
-async function adicionaFinanceiro(valor,valor2,valor3,valor4) {
-    console.log(valor, valor2, valor3, valor4)
-    //Criando o Header
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  
-    //Criando o Body
-    var urlencoded = new URLSearchParams();
-  
-  
-    urlencoded.append("Nome", valor);
-    urlencoded.append("ValorVenda", valor3);
-    urlencoded.append("ValorPedido", valor2);
-    urlencoded.append("ValorLucro", valor4);
-  
-    //instanciando as opção da request
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-  
-    
+async function adicionaFinanceiro(valor) {
+  //Criando o Header
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-    //Envio para a API e retornando para a página
-    return fetch("http://localhost:3000/financeiro/adcionar", requestOptions)
-      .then((response) => response.text())
-      .then((result) => result)
-      .catch((error) => console.log("erro na request: ", error));
-  }
+  //Criando o Body
+  var urlencoded = new URLSearchParams();
 
 
+  urlencoded.append("id_venda_fk", valor[0]);
+  urlencoded.append("ValorVenda", valor[1]);
+  urlencoded.append("ValorPedido", valor[2]);
+  urlencoded.append("ValorLucro", valor[3]);
 
-  async function calcularaLucro() {
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  
-    //Criando o Body
-    var urlencoded = new URLSearchParams();
-  
-  
-    urlencoded.append("ValorLucro", valor);
-
-
-
-    var ValorPedido = parseInt(document.getElementById('ValorPedido').value);
-    var ValorVenda = parseInt(document.getElementById('ValorVenda').value);
-    document.getElementById('ValorLucro').innerHTML = ValorVenda -ValorPedido 
-
- //instanciando as opção da request
- var requestOptions = {
+  //instanciando as opção da request
+  var requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: urlencoded,
@@ -63,4 +26,75 @@ async function adicionaFinanceiro(valor,valor2,valor3,valor4) {
   };
 
 
+
+  //Envio para a API e retornando para a página
+  return fetch("http://localhost:3000/financeiro/adcionar", requestOptions)
+    .then((response) => response.text())
+    .then((result) => result)
+    .catch((error) => console.log("erro na request: ", error));
+}
+
+
+
+
+
+
+
+const getProdutoEstoque = async () => (
+  fetch(`http://localhost:3000/estoque`)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((erro) => console.log("erro na request: " + erro))
+)
+
+const getVendasId = async (id) => (
+  fetch(`http://localhost:3000/vendas/${id}`)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((erro) => console.log("erro na request: " + erro))
+)
+
+// função que lista o financeiro
+const getListar = async () => (
+  fetch(`http://localhost:3000/financeiros`)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((erro) => console.log("erro na request: " + erro))
+)
+
+const getFinanceiroId = async (id) => (
+  fetch(`http://localhost:3000/BuscarId/${id}`)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((erro) => console.log("erro na request: " + erro))
+)
+
+
+
+async function Editar(valor) {
+  //Criando o Header
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  //Criando o Body
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("ValorVenda", valor[1]);
+  urlencoded.append("ValorPedido", valor[2]);
+  urlencoded.append("ValorLucro", valor[3]);
+
+  //instanciando as opção da request
+  var requestOptions = {
+    method: "PATCH",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+
+
+
+  //Envio para a API e retornando para a página
+  return fetch(`http://localhost:3000/financeiro/editar/${valor[0]}`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => result)
+    .catch((error) => console.log("erro na request: ", error));
 }
--- src/modules/Financeiro/script.js
+++ src/modules/Financeiro/script.js
@@ -1,33 +1,137 @@
 
 
- 
 
-  
-  //Submit da tela
-  async function adicionahandlefinan() {
-      
-    let Nome = document.getElementById("name").value
-    let ValorPedido = document.getElementById("Valor_Pedido").value
-    let ValorVenda = document.getElementById("Valor_Venda").value
-  
-    try {
-      
-      //default: sucess = true, error = false;
-      let request = await adicionaFinanceiro(Nome,ValorPedido,ValorVenda,(ValorVenda - ValorPedido));
-  
-      
-    } catch (error) {
-     console.log(error)
+
+
+//Submit da tela
+async function adicionahandlefinan() {
+  let ids = new Array();
+  for (i = 0; i < 4; i++) {
+    let { id } = document.querySelectorAll('td')[i];
+    ids.push(id)
+
+  }
+  try {
+
+    //default: sucess = true, error = false;
+    let request = await adicionaFinanceiro(ids);
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+async function calcularaLucro() {
+  var ValorPedido = document.getElementById('Valor_Pedido').value;
+  var ValorVenda = document.getElementById('Valor_Venda').value;
+  document.getElementById('Valor_Lucro').innerHTML = ValorVenda - ValorPedido
+}
+
+
+
+
+const getProdutoVenda = async () => (await getProdutoEstoque())
+
+const getprodutos = async (result) => {
+
+  let obj = getProdutoVenda()
+  console.log(obj)
+};
+
+
+const teste = async () => {
+  let valor = document.getElementById('buscar').value;
+  let produtoVenda = await getVendasId(valor)
+  let historico = await getProdutoVenda()
+
+  let produtoUnico = produtoVenda[0].produtos;
+  let prod = produtoUnico.split(',');
+  let total = 0;
+  for (let i = 0; i < prod.length; i++) {
+    for (let j = 0; j < historico.length; j++) {
+      if (prod[i] == historico[j].produto) {
+        total += historico[j].valor;
+      }
     }
-  
+  }
+  let td = `<tr>
+   <td id="${valor}">${valor}</td>
+   <td id="${produtoVenda[0].valor}" >${produtoVenda[0].valor}</td>
+   <td id="${total}">${total}</td>
+   <td id="${produtoVenda[0].valor - total}">${produtoVenda[0].valor - total}</td>
+   </tr>`
+  document.getElementById('tabela').innerHTML = td;
+       
+  let bot ='<button class="btn btn-success" type="submit" onclick="adicionahandlefinan()"> Enviar</button>'
+  document.getElementById("bot").innerHTML = bot
+}
 
 
- }
-  
-  
- async function calcularaLucro() {
-    var ValorPedido = parseInt(document.getElementById('Valor_Pedido').value);
-    var ValorVenda = parseInt(document.getElementById('Valor_Venda').value);
-    document.getElementById('Valor_Lucro').innerHTML = ValorVenda -ValorPedido }
-  
+async function teste2() {
+  let table = document.getElementById('tabela');
+  let result = await getListar();
+  result.map((lista) => {
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = lista.id_financeiro_pk;
+    cell4.innerHTML = 'R$' + String(lista.ValorLucro.toFixed(2)).replace('.', ',');
+    cell2.innerHTML = 'R$' + String(lista.ValorVenda.toFixed(2)).replace('.', ',');
+    cell3.innerHTML = 'R$' + String(lista.ValorPedido.toFixed(2)).replace('.', ',');
+    let botao = `<button class="btn btn-success" onclick="listaFinanceiroId(${lista.id_financeiro_pk})">Editar</button>`
+    cell5.innerHTML = botao;
     
+  });
+}
+async function listaFinanceiroId(id) {
+  let result = await getFinanceiroId(id)
+  result.map((lista) => {
+    // lista.ValorLucro = 'R$' + String(lista .toFixed(2)).replace('.', ',');
+    // lista.ValorVenda = 'R$' + String(lista.ValorVenda.toFixed(2)).replace('.', ',');
+    // lista.ValorPedido = 'R$' + String(lista.ValorPedido.toFixed(2)).replace('.', ',');
+  let linha = `<tr>
+  <td><input type="number" id="id"></td>
+  <td><input type="number" id="venda" onchange="calcularLucro()"></td>
+  <td><input type="number" id="pedido" onchange="calcularLucro()"></td>
+  <td><input type="number" id="lucro"></td>
+  </tr>`
+  document.getElementById('tabela').innerHTML = linha;
+  document.getElementById('id').value = lista.id_financeiro_pk
+  document.getElementById('id').disabled = true
+  document.getElementById('venda').value = lista.ValorVenda
+  document.getElementById('pedido').value = lista.ValorPedido
+  document.getElementById('lucro').value = lista.ValorLucro;
+  document.getElementById('lucro').disabled = true
+
+
+});
+
+}
+
+async function editar() {
+  let id = document.getElementById('id').value
+  let venda = document.getElementById('venda').value
+  let pedido = document.getElementById('pedido').value
+  let lucro = document.getElementById('lucro').value
+
+  let valor = new Array(id,venda,pedido,lucro)
+  try {
+    //default: sucess = true, error = false;
+    let request = await Editar(valor);
+
+  } catch (error) {
+    console.log(error)
+  }
+  location.href = "./financeiross.html"
+ 
+}
+function calcularLucro(){
+  let venda = document.getElementById('venda').value;
+  let pedido = document.getElementById('pedido').value;
+  document.getElementById('lucro').value = venda - pedido;
+}
